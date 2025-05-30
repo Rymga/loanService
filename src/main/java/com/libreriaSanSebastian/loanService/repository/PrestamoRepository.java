5649385ec1e87409e814ccfcfe13443f6eb82bff
fr <fr.choque@duocuc.cl>
2025-05-30 05:51:55 -0400

    repository

--- src/main/java/com/libreriaSanSebastian/loanService/repository/PrestamoRepository.java
+++ src/main/java/com/libreriaSanSebastian/loanService/repository/PrestamoRepository.java
@@ -1,4 +1,15 @@
 package com.libreriaSanSebastian.loanService.repository;
 
-public class PrestamoRepository {
+
+import com.libreriaSanSebastian.loanService.modelo.Prestamo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PrestamoRepository extends JpaRepository<Prestamo, Long> {
+    List<Prestamo> findByUsuarioId(Long usuarioId);
+    List<Prestamo> findByLibroId(Long libroId);
+    List<Prestamo> findByEstado(String estado);
 }
