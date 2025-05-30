c67bb016c65b0b97f1109bf7d752900280353192
fr <fr.choque@duocuc.cl>
2025-05-30 06:13:51 -0400

    repository

--- src/main/java/com/libreriaSanSebastian/loanService/service/PrestamoService.java
+++ src/main/java/com/libreriaSanSebastian/loanService/service/PrestamoService.java
@@ -1,4 +1,30 @@
 package com.libreriaSanSebastian.loanService.service;
 
+import com.libreriaSanSebastian.loanService.modelo.Prestamo;
+import com.libreriaSanSebastian.loanService.repository.PrestamoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PrestamoService {
+
+    @Autowired
+    private PrestamoRepository prestamoRepository;
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public List<Prestamo> listarTodos() {
+        return prestamoRepository.findAll();
+    }
+
+    public Optional<Prestamo> buscarPorId(Long id) {
+        return prestamoRepository.findById(id);
+    }
+
+    public List<Prestamo> buscarPorUsuario(Long usuarioId) {
+        return prestamoRepository.findByUsuarioId(usuarioId);
+    }
 }
